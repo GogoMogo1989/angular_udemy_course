da8c08be9d3928e186cd6b67daeda61922062f9a
GogoMogo1989 <kovacsfarkasdavid@gmail.com>
2023-09-25 15:37:28 +0200

    third practise

--- angular-udemy-course/src/app/servers/servers.component.ts
+++ angular-udemy-course/src/app/servers/servers.component.ts
@@ -3,12 +3,23 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-servers',
   templateUrl: './servers.component.html',
-  styleUrls: ['./servers.component.css'],
+  styles: [
+    `
+      .online {
+        background-color: blue;
+      }
+    `,
+  ],
 })
 export class ServersComponent {
   allowNewServer: boolean = false;
   serverCreationStatus: string = 'No server was created';
   serverName = 'Tester';
+  serverCreated: boolean = false;
+  servers = ['Testserver', 'Testserver 2'];
+  secretPassword: string = 'titkos';
+  disabledValue: boolean = false;
+  numberValue: number = 0;
 
   constructor() {
     setTimeout(() => {
@@ -17,10 +28,17 @@ export class ServersComponent {
   }
 
   onCreateServer() {
+    this.serverCreated = true;
+    this.servers.push(this.serverName);
     this.serverCreationStatus = 'Server was created!' + this.serverName;
   }
 
   onUpdateServerName(event: any) {
     this.serverName = event.target.value;
+  }
+
+  buttonClicked(event: any) {
+    this.disabledValue = true;
+    this.numberValue++;
   }
 }
