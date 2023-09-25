a56481247fa9a40c605812ab2ba322afc9e0e5f6
GogoMogo1989 <kovacsfarkasdavid@gmail.com>
2023-09-25 14:21:15 +0200

    35. assigment

---
+++ angular-udemy-course/src/app/servers/servers.component.ts
@@ -0,0 +1,42 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-servers',
+  templateUrl: './servers.component.html',
+  styleUrls: ['./servers.component.css'],
+})
+export class ServersComponent {
+  /*   allowNewServer: boolean = false;
+  serverCreationStatus: string = 'No server was created';
+  serverName = 'Tester';
+
+  constructor() {
+    setTimeout(() => {
+      this.allowNewServer = true;
+    }, 2000);
+  }
+
+  onCreateServer() {
+    this.serverCreationStatus = 'Server was created!' + this.serverName;
+  }
+
+  onUpdateServerName(event: any) {
+    this.serverName = event.target.value;
+  } */
+
+  inputValue: string = '';
+  buttonDisabled: boolean = true;
+  inputValue2: string = '';
+
+  onInputChange() {
+    if (this.inputValue !== '') {
+      this.buttonDisabled = false;
+    } else {
+      this.buttonDisabled = true;
+    }
+  }
+
+  buttonClicked() {
+    this.inputValue2 = this.inputValue;
+  }
+}
